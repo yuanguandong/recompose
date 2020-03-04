46905c28c903333c5df63a0ac876f19d5cfdb6e3
袁官东 <yuangd@gantsoftware.com>
2020-03-04 10:40:25 +0800

    fix(App.js): 修复一个问题

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withState } from 'recompose'
+import { withState } from 'recompose';
 
 const Testable = () => {
   useEffect(() => {
-    console.log(1)
-  }, [])
-}
+    console.log(1);
+  }, []);
+};
 
 const App = () => {
-  Testable()
-  return (
-    <div className="App">
-      1
-    </div>
-  );
-}
-
-
+  Testable();
+  return <div className="App">1</div>;
+};
 
 export default App;
